55e703fe57f9e9414bf9b0c1ab2eddf14db46855
Adam Twardoch <adam@twardoch.com>
2026-04-24 17:27:17 +0200

    v1.0.1

--- src/mcp_synchro/_version.py
+++ src/mcp_synchro/_version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = '0.1.dev1+g0fd2f0aaf.d20260325'
-__version_tuple__ = version_tuple = (0, 1, 'dev1', 'g0fd2f0aaf.d20260325')
+__version__ = version = '1.0.1.dev0+g9f35176b9.d20260424'
+__version_tuple__ = version_tuple = (1, 0, 1, 'dev0', 'g9f35176b9.d20260424')
 
 __commit_id__ = commit_id = None
